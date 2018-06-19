6b9b59f930d43809f0d58decca57a4cd3cb58f5e
lpitao <1549920201@qq.com>
2018-06-19 17:25:22 +0800

    example

---
+++ pages/form/form.js
@@ -0,0 +1,94 @@
+// pages/form/form.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    diqu: [1, 2, 3],
+    diqu2: [1, 2, 3],
+    diqu1index: 0,
+    diqu2index: 0,
+    hangye1: [1, 4, 3],
+    hangye2: [1, 2, 3],
+    hangye1index: 0,
+    hangye2index: 0,
+
+    biao1: [1, 4, 3],
+    biao1: [1, 2, 3],
+    biao1index: 0,
+    biao1index: 0,
+    biao2: [1, 4, 3],
+    biao2: [1, 2, 3],
+    biao2index: 0,
+    biao2index: 0,
+    biao3: [1, 4, 3],
+    biao3: [1, 2, 3],
+    biao3index: 0,
+    biao3index: 0,
+    biao4: [1, 4, 3],
+    biao4: [1, 2, 3],
+    biao4index: 0,
+    biao4index: 0,
+
+    lirun: [
+      '0-2', '2-4', '4-6', '6-8', '8-10', '10以上', '无'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
